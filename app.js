8ea109526ab8be3195454b6c494fec6d6eb76cf8
Charles Marks <clmarks15@gmail.com>
2018-12-20 17:28:25 -0500

    part 2 complete

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 //alert("fourth");
 
+// Init weather object
+
+const weather = new Weather ('30273');
+
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+    function getWeather() {
+        weather.getWeather()
+            .then(results => {
+                console.log(results);
+            })
+            .catch(err => console.log(err));
+    }
--- weather.js
+++ weather.js
@@ -1,2 +1,28 @@
 //alert("second");
 
+class Weather   {
+    constructor(zip){
+        this.apiKey = 'a7a0de22a1b91e73218e6f5d9ad61e5c';
+        this.zip = zip;
+    }
+
+    // Fetch weather from API
+    async getWeather()  {
+        const response = await fetch
+        (`https://api.openweathermap.org/data/2.5/weather?zip=${this.zip},us&units=imperial&APPID=${this.apiKey}`);
+
+        /* this worked in browser
+        https://api.openweathermap.org/data/2.5/weather?zip=30273,us&APPID=a7a0de22a1b91e73218e6f5d9ad61e5c
+        */
+
+        const responseData = await response.json();
+
+        return responseData.main;
+    }
+
+    // Change weather location
+    changeLocation(zip) {
+        this.zip = zip;
+    }
+}
+
